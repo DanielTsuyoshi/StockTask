ace8371fd4303c36acf68934306bdba04a126814
DanielTsuyoshi <110398603+DanielTsuyoshi@users.noreply.github.com>
2023-10-18 15:32:50 -0300

    Add files via upload

---
+++ src/test/java/br/com/fiap/stocktask/StocktaskApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.stocktask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocktaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
